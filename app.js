66f38104b00d970430c279a4c61231d23dbff09d
Jiehla Dacara <jiedacara@gmail.com>
2025-03-07 01:11:54 -0700

    add media queries for responsive design, update input validation

---
+++ app.js
@@ -0,0 +1,5 @@
+const firstName = document.getElementById('first-name');
+const lastName = document.getElementById('last-name');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+
