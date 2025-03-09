4d9a9ac19b244b0c6ba660307ada2c74183cf302
Jiehla Dacara <jiedacara@gmail.com>
2025-03-09 03:14:47 -0700

    Add input validation for trial button and update button ID

--- app.js
+++ app.js
@@ -2,4 +2,15 @@ const firstName = document.getElementById('first-name');
 const lastName = document.getElementById('last-name');
 const email = document.getElementById('email');
 const password = document.getElementById('password');
+const inputs = document.querySelectorAll('input');
+const trialBtn = document.getElementById('trial-btn');
 
+
+
+trialBtn.addEventListener('click', () => {
+    inputs.forEach(input => {
+        if (!input.value.trim()) {
+            input.style.border = '1px solid var(--red)';
+        }
+    });
+})
