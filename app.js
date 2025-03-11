8b845bba758f34e9ea71e2001f4a5e53acecff11
Jiehla Dacara <jiedacara@gmail.com>
2025-03-11 02:32:29 -0700

    Add error handling and remove placeholders on focus

--- app.js
+++ app.js
@@ -6,11 +6,17 @@ const inputs = document.querySelectorAll('input');
 const trialBtn = document.getElementById('trial-btn');
 
 
-
 trialBtn.addEventListener('click', () => {
     inputs.forEach(input => {
         if (!input.value.trim()) {
             input.style.border = '1px solid var(--red)';
+            input.style.background = 'url(../images/icon-error.svg) no-repeat right 20px center';
         }
-    });
+    })
+})
+
+inputs.forEach(input => {
+    input.addEventListener('focus', () => {
+        input.placeholder = '';
+    })
 })
